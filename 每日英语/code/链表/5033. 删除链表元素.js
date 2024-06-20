361dc97d98edc9bc386661cbdaa671c38c2c1f69
吴习章 <wuxz14@vanke.com>
2024-06-20 20:26:54 +0800

    feat: 20240620 删除链表元素

---
+++ 每日英语/code/链表/5033. 删除链表元素.js
@@ -0,0 +1,58 @@
+function ListNode(val, next) {
+  this.val = val;
+  this.next = next;
+}
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = val;
+ *     this.next = next;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} value
+ * @returns {ListNode}
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = val;
+ *     this.next = next;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} value
+ * @returns {ListNode}
+ */
+var removeElements = function (head, val) {
+  if (!head || !val) return head;
+
+  var standNode = new ListNode(null, head);
+  var prev = standNode;
+  var node = head;
+  while (node) {
+    console.log("prev", prev);
+    console.log("standNode", standNode);
+    if (node.val === val) {
+      prev.next = node.next;
+    } else {
+      // prev.next = node;
+      prev = prev.next; // 关键在这里 prev = prev.next  而不是 prev.next = node
+    }
+    node = node.next;
+  }
+
+  return standNode.next;
+};
+
+var head01 = {
+  val: 1,
+  next: { val: 3, next: { val: 1, next: { val: 3, next: null } } },
+};
+var head = { val: 1, next: head01 };
+
+var a = removeElements(head, 1);
+console.log("a", a);
