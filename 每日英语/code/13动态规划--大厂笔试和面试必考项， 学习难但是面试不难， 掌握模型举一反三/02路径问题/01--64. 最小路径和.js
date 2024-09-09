addf74bbcda235658aca59efed6b37b78929897c
吴习章 <wuxz14@vanke.com>
2024-09-09 12:03:08 +0800

    华为 OD Day07--动态规划之路径问题模型--上午

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/02路径问题/01--64. 最小路径和.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/02路径问题/01--64. 最小路径和.js
@@ -8,6 +8,17 @@ var minPathSum = function (grid) {
   var dp = new Array(m)
     .fill()
     .map(() => new Array(n).fill(Number.MAX_SAFE_INTEGER));
+  // var dp = new Array(m).fill().map(() => new Array(n).fill(0));
+
+  // 初始化数据
+  // var len = 0;
+  // for(let i = 0; i < m; i++){
+  //     dp[i][0] += grid[i][0]
+  // }
+  // len = 0;
+  // for(let j = 0; j < n; j++){
+  //     dp[0][j] += grid[0][j]
+  // }
 
   var len = 0;
   for (let i = 0; i < m; i++) {
@@ -20,9 +31,10 @@ var minPathSum = function (grid) {
     dp[0][j] = len;
   }
 
+  // 状态转移方程
   for (let i = 1; i < m; i++) {
     for (let j = 1; j < n; j++) {
-      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+      dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
     }
   }
 
