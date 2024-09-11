1b498baf8035104390b2964a43f11ca1efdb7440
吴习章 <wuxz14@vanke.com>
2024-09-11 09:30:32 +0800

    华为 OD Day07--动态规划之最长递增子序列

---
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/04爬楼梯问题/01--70. 爬楼梯.js
@@ -0,0 +1,21 @@
+// dp算法
+var climbStairs = function (n) {
+  var dp = new Array(n).fill(0);
+
+  dp[0] = 1;
+  dp[1] = 2;
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n - 1];
+};
+
+// 递归超时
+// var climbStairs = function (n) {
+//   if (n === 1) return 1;
+//   if (n === 2) return 2;
+
+//   return climbStairs(n - 1) + climbStairs(n - 2);
+// };
