39a6ca4648f25caa43290c7922695f369bc674a6
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 16:55:48 +0800

    Update 02--LCR 166. 珠宝的最高价值.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/02路径问题/02--LCR 166. 珠宝的最高价值.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/02路径问题/02--LCR 166. 珠宝的最高价值.js
@@ -2,29 +2,24 @@
  * @param {number[][]} frame
  * @return {number}
  */
-var jewelleryValue = function (frame) {
-  var m = frame.length;
-  var n = frame[0].length;
-  var dp = new Array(m)
-    .fill()
-    .map(() => new Array(n).fill(Number.MIN_SAFE_INTEGER));
+var jewelleryValue = function(frame) {
+    var m = frame.length;
+    var n = frame[0].length;
+    var dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
 
-  var len = 0;
-  for (let i = 0; i < m; i++) {
-    len += frame[i][0];
-    dp[i][0] = len;
-  }
-  len = 0;
-  for (let j = 0; j < n; j++) {
-    len += frame[0][j];
-    dp[0][j] = len;
-  }
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + frame[i][j];
+    dp[0][0] = frame[0][0];
+    for(let i = 1; i < m; i++){
+        dp[i][0] = dp[i-1][0] + frame[i][0];
     }
-  }
+    for(let j = 1; j < n; j++){
+        dp[0][j] = dp[0][j-1] + frame[0][j];
+    }
 
-  return dp[m - 1][n - 1];
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j < n; j++){
+            dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]) + frame[i][j]; 
+        }
+    }
+
+    return dp[m-1][n-1];
 };
