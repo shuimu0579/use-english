84858b07bd935dab4addfe28f5e7516895c03f5c
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 14:47:17 +0800

    Update 01--多做几遍--416. 分割等和子集--0-1背包--可行.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/01--多做几遍--416. 分割等和子集--0-1背包--可行.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/01--多做几遍--416. 分割等和子集--0-1背包--可行.js
@@ -5,31 +5,27 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canPartition = function (nums) {
-  var sum = nums.reduce((a, b) => a + b, 0);
-  if (sum % 2 !== 0) return false;
+var canPartition = function(nums) {
+    let sum = nums.reduce((a, b) => a + b, 0);
+    if(sum % 2 !== 0) return false;
 
-  var m = nums.length;
-  // var n = sum % 2;
-  var n = sum / 2;
-  var dp = new Array(m).fill(null).map(() => new Array(n + 1).fill(false));
+    var target  = sum / 2;
+    var m = nums.length;
+    var n = target;
+    var dp = new Array(m).fill(null).map(() => new Array(n + 1).fill(false));
 
-  dp[0][0] = true;
-  if (nums[0] <= n) {
-    dp[0][nums[0]] = true;
-  }
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 0; j <= n; j++) {
-      if (
-        dp[i - 1][j] === true ||
-        (j - nums[i] >= 0 && dp[i - 1][j - nums[i]] === true)
-      ) {
-        // dp[i][j] === true;
-        dp[i][j] = true;
-      }
+    dp[0][0] = true;
+    if(nums[0] <= n){
+        dp[0][nums[0]] = true;
     }
-  }
 
-  return dp[m - 1][n];
+    for(let i = 1; i < m; i++){
+        for(let j = 0; j <= n; j++){
+            if(dp[i-1][j] === true || (j-nums[i] >= 0 && dp[i-1][j-nums[i]] === true) ){
+                dp[i][j] = true
+            }
+        }
+    }
+
+    return dp[m-1][n];
 };
