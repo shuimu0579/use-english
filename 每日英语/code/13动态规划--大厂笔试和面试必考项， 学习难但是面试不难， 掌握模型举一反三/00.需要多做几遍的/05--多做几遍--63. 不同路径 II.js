53c2aeef45b673e648a5ce9884ea68870a3ff13f
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 17:41:15 +0800

    Update 05--多做几遍--63. 不同路径 II.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/05--多做几遍--63. 不同路径 II.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/05--多做几遍--63. 不同路径 II.js
@@ -4,55 +4,41 @@
  * @param {number[][]} obstacleGrid
  * @return {number}
  */
-var uniquePathsWithObstacles = function (obstacleGrid) {
-  var m = obstacleGrid.length;
-  var n = obstacleGrid[obstacleGrid.length - 1].length;
-  var dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    var m = obstacleGrid.length;
+    var n = obstacleGrid[0].length;
+    var dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
 
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
-  }
-  for (let i = 0; i < m; i++) {
-    if (obstacleGrid[i][0] === 1) {
-      let index = i;
-      while (index < m) {
-        // dp[i][0] = 0;
-        dp[index][0] = 0;
-        index++;
-      }
+    for(let i = 0; i < m; i++){
+        dp[i][0] = 1;
+        if(obstacleGrid[i][0] === 1){
+            for(let j = i; j < m; j++){
+                dp[j][0] = 0;
+            }
+            break;
+        }
     }
-  }
-
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
-  for (let j = 0; j < n; j++) {
-    if (obstacleGrid[0][j] === 1) {
-      let index = j;
-      while (index < n) {
-        // dp[0][j] = 0;
-        dp[0][index] = 0;
-        index++;
-      }
+    for(let i = 0; i < n; i++){
+        dp[0][i] = 1;
+        if(obstacleGrid[0][i] === 1){
+            for(let j = i; j < m; j++){
+                dp[0][j] = 0;
+            }
+            break;
+        }
     }
-  }
 
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      // if(obstacleGrid[i][j-1] === 1){
-      //     dp[i][j] += dp[i-1][j]
-      // }else if(obstacleGrid[i-1][j] === 1){
-      //     dp[i][j] += dp[i][j-1]
-      // }else {
-      //     dp[i][j] += dp[i-1][j] + dp[i][j-1]
-      // }
-      if (obstacleGrid[i][j] === 1) {
-        dp[i][j] = 0;
-      } else {
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-      }
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j < n; j++){
+            if(obstacleGrid[i][j-1] === 1){
+                dp[i][j-1] = 0;
+            }
+            if(obstacleGrid[i-1][j] === 1){
+                dp[i-1][j] = 0;
+            }
+            dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
     }
-  }
 
-  return dp[m - 1][n - 1];
+    return obstacleGrid[m-1][n-1] === 1 ? 0 : dp[m-1][n-1];
 };
