30f646fd52f9e29c5ea5f0f1234ab43246cc3bde
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 15:59:29 +0800

    Update 03--多做几遍--322. 零钱兑换.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/03--多做几遍--322. 零钱兑换.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/03--多做几遍--322. 零钱兑换.js
@@ -1,33 +1,31 @@
-// https://leetcode.cn/problems/coin-change/description/
+// https://leetcode.cn/problems/coin-change/
 
 /**
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function (coins, amount) {
-  var m = coins.length;
-  var n = amount;
-  var dp = new Array(m)
-    .fill(null)
-    .map(() => new Array(n + 1).fill(Number.MAX_SAFE_INTEGER));
+var coinChange = function(coins, amount) {
+    var m = coins.length;
+    var n = amount;
+    var dp = new Array(m).fill(null).map( () => new Array(n+1).fill(Number.MAX_SAFE_INTEGER) )
 
-  let k = amount / coins[0];
-  for (let i = 0; i <= k; i++) {
-    dp[0][i * coins[0]] = i;
-  }
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 0; j <= n; j++) {
-      let k = j / coins[i];
-      let min = Number.MAX_SAFE_INTEGER;
-      for (let r = 0; r <= k; r++) {
-        min = Math.min(min, dp[i - 1][j - r * coins[i]] + r);
-      }
-
-      dp[i][j] = min;
+    let k = Math.floor(n / coins[0]);
+    for(let i = 0; i <= k; i++){
+        dp[0][i * coins[0]] = i;
     }
-  }
 
-  return dp[m - 1][n] !== Number.MAX_SAFE_INTEGER ? dp[m - 1][n] : -1;
+    for(let i = 1; i < m; i++){
+        for(let j = 0; j <= n; j++){
+            let k = Math.floor(j / coins[i]);
+            let min = Number.MAX_SAFE_INTEGER;
+            for(let r = 0; r <= k; r++){
+                min = Math.min(min, dp[i-1][j - r * coins[i]] + r)
+            }
+
+            dp[i][j] = min;
+        }
+    }
+
+    return dp[m-1][n] !== Number.MAX_SAFE_INTEGER ? dp[m-1][n] : -1;
 };
