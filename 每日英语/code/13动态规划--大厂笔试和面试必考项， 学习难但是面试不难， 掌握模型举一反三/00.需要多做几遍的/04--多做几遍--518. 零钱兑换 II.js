8a8c76423ce2f523438ac9459a2d007a1e447381
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 16:00:31 +0800

    Update 04--多做几遍--518. 零钱兑换 II.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/04--多做几遍--518. 零钱兑换 II.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/04--多做几遍--518. 零钱兑换 II.js
@@ -5,25 +5,25 @@
  * @param {number[]} coins
  * @return {number}
  */
-var change = function (amount, coins) {
-  var m = coins.length;
-  var n = amount;
-  var dp = new Array(m).fill(null).map(() => new Array(n + 1).fill(0));
+var change = function(amount, coins) {
+    var m = coins.length;
+    var n = amount;
+    var dp = new Array(m).fill(null).map(() => new Array(n+1).fill(0));
 
-  var k = n / coins[0];
-  for (let i = 0; i <= k; i++) {
-    // dp[0][i] = 1;
-    dp[0][i * coins[0]] = 1;
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 0; j <= n; j++) {
-      let k = j / coins[i];
-      for (let r = 0; r <= k; r++) {
-        dp[i][j] += dp[i - 1][j - r * coins[i]];
-      }
-      // dp[i][j] = dp[i-1][j] + dp[i-1][j-coins[i]]
+    dp[0][0] = 1;
+    let k = Math.floor(n / coins[0])
+    for(let i = 0; i <= k; i++){
+        dp[0][i * coins[0]] = 1;
     }
-  }
 
-  return dp[m - 1][n];
+    for(let i = 1; i < m; i++){
+        for(let j = 0; j <= n; j++){
+            let k = Math.floor(j / coins[i])
+            for(let r = 0; r <= k; r++){
+                dp[i][j] += dp[i-1][j - r * coins[i]]
+            }
+        }
+    }
+
+    return dp[m-1][n]
 };
