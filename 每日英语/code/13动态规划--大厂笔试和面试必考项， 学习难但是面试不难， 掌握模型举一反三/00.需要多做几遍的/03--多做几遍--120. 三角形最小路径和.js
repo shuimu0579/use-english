75d87f0f387eaa0e7301de4273f365f87b2ec488
shuimu0579 <shuimu0579@gmail.com>
2024-10-24 18:07:20 +0800

    Update 03--多做几遍--120. 三角形最小路径和.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/03--多做几遍--120. 三角形最小路径和.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/03--多做几遍--120. 三角形最小路径和.js
@@ -4,37 +4,27 @@
  * @param {number[][]} triangle
  * @return {number}
  */
-var minimumTotal = function (triangle) {
-  var m = triangle.length;
-  var n = triangle[triangle.length - 1].length;
-  var dp = new Array(m)
-    .fill(null)
-    .map(() => new Array(n).fill(Number.MAX_SAFE_INTEGER));
+var minimumTotal = function(triangle) {
+    var m = triangle.length;
+    var n = triangle[m-1].length;
+    var dp = new Array(m).fill(null).map(() => new Array(n).fill(Number.MAX_SAFE_INTEGER));
 
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 0;
-  }
-
-  var sum = 0;
-  for (let i = 0; i < m; i++) {
-    sum += triangle[i][0];
-    dp[i][0] = sum;
-  }
-
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j <= i; j++) {
-      if (j === i) {
-        dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
-      } else {
-        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
-      }
+    dp[0][0] = triangle[0][0];
+    for(let i = 1; i < m; i++){
+        dp[i][0] = dp[i-1][0] + triangle[i][0];
     }
-  }
 
-  var min = Number.MAX_SAFE_INTEGER;
-  for (let i = 0; i < m; i++) {
-    min = Math.min(min, dp[m - 1][i]);
-  }
 
-  return min;
+    for(let i = 1; i < m; i++){
+        for(let j = 1; j <=i;j++){
+            dp[i][j] = Math.min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j];  
+        }
+    }
+
+    // return dp[m-1][n-1];
+    var min = Number.MAX_SAFE_INTEGER;
+    for(let i = 0; i < n; i++){
+        min = Math.min(min, dp[m-1][i]);
+    }
+    return min;
 };
