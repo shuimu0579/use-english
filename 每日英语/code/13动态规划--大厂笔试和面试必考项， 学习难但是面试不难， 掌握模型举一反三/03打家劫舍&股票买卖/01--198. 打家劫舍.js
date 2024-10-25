0ddf1804466f350d4429cb499632df8e6201aa93
shuimu0579 <shuimu0579@gmail.com>
2024-10-25 10:03:55 +0800

    Update 01--198. 打家劫舍.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/03打家劫舍&股票买卖/01--198. 打家劫舍.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/03打家劫舍&股票买卖/01--198. 打家劫舍.js
@@ -2,21 +2,19 @@
  * @param {number[]} nums
  * @return {number}
  */
-var rob = function (nums) {
-  var m = nums.length;
-  var n = 2;
-  var dp = new Array(m)
-    .fill(null)
-    .map(() => new Array(n).fill(Number.MIN_SAFE_INTEGER));
+var rob = function(nums) {
+    var m = nums.length;
+    var n = 2;
+    var dp = new Array(m).fill(null).map(() => new Array(n).fill(0));
 
-  // 0 不偷 1 偷
-  dp[0][0] = 0;
-  dp[0][1] = nums[0];
+    // 0代表不偷, 1代表偷
+    dp[0][0] = 0;
+    dp[0][1] = nums[0];
 
-  for (let i = 1; i < m; i++) {
-    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-    dp[i][1] = dp[i - 1][0] + nums[i];
-  }
+    for(let i = 1; i < m; i++){
+        dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1]);
+        dp[i][1] = dp[i-1][0] + nums[i];
+    }
 
-  return Math.max(dp[m - 1][0], dp[m - 1][1]);
+    return Math.max(dp[m-1][0], dp[m-1][1]);
 };
