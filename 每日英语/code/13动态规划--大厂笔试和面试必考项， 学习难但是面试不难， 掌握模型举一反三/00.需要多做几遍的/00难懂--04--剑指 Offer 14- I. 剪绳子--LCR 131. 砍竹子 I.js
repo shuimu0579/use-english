143be7853ba3d4466e20cd2f66496e0ae80b138d
shuimu0579 <shuimu0579@gmail.com>
2024-10-25 15:50:30 +0800

    Update 00难懂--04--剑指 Offer 14- I. 剪绳子--LCR 131. 砍竹子 I.js

--- 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/00难懂--04--剑指 Offer 14- I. 剪绳子--LCR 131. 砍竹子 I.js
+++ 每日英语/code/13动态规划--大厂笔试和面试必考项， 学习难但是面试不难， 掌握模型举一反三/00.需要多做几遍的/00难懂--04--剑指 Offer 14- I. 剪绳子--LCR 131. 砍竹子 I.js
@@ -9,66 +9,22 @@
  * @return {number}
  */
 
-// 优先这种解法
-// 必须切一刀，1 2 3 特殊处理， n >= 4 的时候，切一刀 一定 大于等于 不切一刀
-// for(let j = 1; j <= i; j++)  1<= j < i, 代表一定切了一刀， j === i 代表一刀都没有切。
-// 但是通过分析，n >= 4 的时候，切一刀 一定 大于等于 不切一刀. 所以取max的时候,一定是取到了切了一刀的值
-// 所以for(let j = 1; j <= i; j++),而不是for(let j = 1; j < i; j++)
-/**
- * @param {number} bamboo_len
- * @return {number}
- */
-/**
- * @param {number} bamboo_len
- * @return {number}
- */
-var cuttingBamboo = function (bamboo_len) {
-  var m = bamboo_len;
-  // 竹子长度为 1,2,3，被砍时，产生的最大长度。
-  if (m === 1) return 1;
-  if (m === 2) return 1;
-  if (m === 3) return 2;
+var cuttingBamboo = function(bamboo_len) {
+    if(bamboo_len === 2) return 1;
+    if(bamboo_len === 3) return 2;
 
-  // dp[i]代表长度为i的情况下，不管有没有切割，竹子的最大长度。
-  var dp = new Array(m + 1).fill(0);
-  dp[0] = 0;
-  dp[1] = 1;
-  dp[2] = 2;
-  dp[3] = 3;
+    var n = bamboo_len;
+    var dp = new Array(n+1).fill(1);
 
-  for (let i = 4; i <= m; i++) {
-    let max = Number.MIN_SAFE_INTEGER;
-    for (let j = 1; j <= i; j++) {
-      max = Math.max(max, j * dp[i - j]);
+    dp[0] = 1;
+
+    for(let i = 1; i < n+1; i++){
+        var max = Number.MIN_SAFE_INTEGER;
+        for(let j = 1; j <= i; j++){
+            max =  Math.max(max, j * dp[i-j]);
+        }
+        dp[i] = max; 
     }
-    dp[i] = max;
-  }
 
-  return dp[m];
-};
-
-// 下面这种解法不好理解
-var cuttingBamboo = function (bamboo_len) {
-  var n = bamboo_len;
-  // var dp = new Array(n+1).fill(1);
-  var dp = new Array(n + 1).fill(0);
-
-  // dp[0] = 1;
-  dp[0] = 0;
-  dp[1] = 1;
-  for (let i = 2; i < n + 1; i++) {
-    // 一定要初始化为0
-    var max = 0;
-    // for(let j = 1; j <= i; j++){
-    for (let j = 1; j < i; j++) {
-      // max = Math.max(max, j * dp[n-j])
-
-      // 切割之后不再切割j*(i-j)，切割之后继续切割j * dp[i-j],
-      // max = Math.max(max, j * dp[i-j])
-      max = Math.max(max, Math.max(j * (i - j), j * dp[i - j]));
-    }
-    dp[i] = max;
-  }
-
-  return dp[n];
+    return dp[n];
 };
